ef842e6c9915a994351a457c5fe7fdd75f2e5e99
AliHassanzadeh79 <alihassan.gerashi@gmail.com>
2025-02-12 08:17:27 +0330

    creat standard_book_list url

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.books),
+    path('list', views.book_list),
+    path('standard-list', views.standard_book_list),
     path('detail', views.detail),
 ]
--- book/views.py
+++ book/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-book_list = [
-    {"id":1, "name":"book1", "writer":"Sadiq Hedayat"},
-    {"id":2, "name":"book2", "writer":"Jalal Al Ahmad"},
-    {"id":3, "name":"book3", "writer":"Simin Daneshwar"}
+books = [
+    {"id":1, "name":"The Blind Owl", "writer":"Sadegh Hedayat"},
+    {"id":2, "name":"The School Principal", "writer":"Jalal Al-e Ahmad"},
+    {"id":3, "name":"Savushun", "writer":"Simin Daneshvar"},
+    {"id":4, "name":"I'll Turn Off the Lights", "writer":"Zoya Pirzad"},
+    {"id":5, "name":"Kelidar", "writer":"Mahmoud Dowlatabadi"},
+    {"id":6, "name":"Symphony of the Dead", "writer":"Abbas Maroufi"},
+    {"id":7, "name":"The Empty Place of Soluch", "writer":"Mahmoud Dowlatabadi"},
+    {"id":8, "name":"Forty Short Letters to My Wife", "writer":"Nader Ebrahimi"},
 ]
 
-def books (request):
-    data = ''.join(f"id: {item['id']} , name: {item['name']} , writer: {item['writer']}<br>" for item in book_list)
+def book_list (request):
+    data = ''.join(f"id: {item['id']} , name: {item['name']} , writer: {item['writer']}<br>" for item in books)
     return HttpResponse(data)
+
+def standard_book_list (request):
+    return render(request,"book/list.html" , context={"books":books})
 
 def detail (request):
     return HttpResponse('book detial!')
